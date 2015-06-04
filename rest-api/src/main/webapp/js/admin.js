1afedef45124400f2d2bf8d228627ea88492ac63
franck2 <f.boncler@gmail.com>
2015-06-04 15:16:31 +0200

    add graph and view of UE in admin interface

--- rest-api/src/main/webapp/js/admin.js
+++ rest-api/src/main/webapp/js/admin.js
@@ -1,7 +1,8 @@
 var divergencesValues = [{data: [], lines: { show: true, steps: true }, label: "GDTot"}];
+var plotStep, plotCourbe;
 
 $(document).ready(function(){
-	plot = $.plot("#placeholder", 
+	plotStep = $.plot("#stepGraph", 
 
 		divergencesValues,
 	{
@@ -11,16 +12,35 @@ $(document).ready(function(){
 		},
 		legend: {
 			show: true,
-			container: $("#legend"),
+			position: "ne",
+			margin:[-60, 0],
 			noColumns: 1,
 		}
 	}
-);
-});
+	);
+	
+		plotCourbe = $.plot("#courbe", 
 
-// Add the Flot version string to the footer
+		divergencesValues,
+	{
+		xaxis: {
+			mode: "time",
+			timeformat: "%Y-%m-%d %H:%M:%S"
+		},
+		legend: {
+			show: true,
+			position: "ne",
+			margin:[-60, 0],
+			noColumns: 1,
+		}
+	}
+	);
+	// Add the Flot version string to the footer
 
 $("#footer").prepend("Flot " + $.plot.version + " &ndash; ");
+});
+
+
 	
 
 function addDivergenceForUsers(){
@@ -33,8 +53,34 @@ function getUsers(){
 
 function updateGraph(){
 	//TODO put in dataAndLabelForGraph the data of all users
-	plot.setData(dataAndLabelForGraph);
-	plot.setupGrid();
-	plot.draw();
+	plotStep.setData(dataAndLabelForGraph);
+	plotStep.setupGrid();
+	plotStep.draw();
 }
 
+(function(){
+	var app = angular.module("Admin", []);
+	
+	app.controller("AdminController", function(){
+		this.tab = 1;
+
+		
+		this.users = ["GDTot", "user1", "user2", "user3", "user4", "user5", "user6", "user7", "user8", "user9", "user10", "user11", "user12", "user13", "user14", "user15", "user16"];
+		this.selectTab = function(setTab){
+			this.tab = setTab;
+		};
+		
+		this.visibilityStepGraph = function(bool){
+			plotStep.resize();
+			plotStep.setupGrid();
+			plotStep.draw();
+		};
+		
+		this.visibilityCourbe = function(bool){
+			plotCourbe.resize();
+			plotCourbe.setupGrid();
+			plotCourbe.draw();
+		};
+	});
+	
+})();
