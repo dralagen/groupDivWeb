6f935c3f09caf0cf3fea0d20d071da071befa9d9
Dralagen <dralagen@dralagen.fr>
2015-06-04 16:08:41 +0200

    add api method commit review and pull on user

--- rest-api/src/main/java/fr/dralagen/groupDiv/restApi/ActionApi.java
+++ rest-api/src/main/java/fr/dralagen/groupDiv/restApi/ActionApi.java
@@ -22,18 +22,44 @@ import fr.dralagen.groupDiv.model.UE;
 )
 public class ActionApi {
 
-    @ApiMethod(name = "action.commit.ue", httpMethod = ApiMethod.HttpMethod.POST, path = "action/commit/ue")
-    public LogAction commitUe(@Named("content") String content, UE ue) {
+  @ApiMethod(name = "action.commit.ue", httpMethod = ApiMethod.HttpMethod.POST, path = "action/commit/ue")
+  public LogAction commitUe(@Named("content") String content, UE ue) {
 
-      //TODO dralagen 6/4/15 : Add new version of UE
-      //TODO dralagen 6/4/15 : Log Commit UE
-      //TODO dralagen 6/4/15 : Update divergence
+    //TODO dralagen 6/4/15 : Add new version of UE
+    //TODO dralagen 6/4/15 : Log Commit UE
+    //TODO dralagen 6/4/15 : Update divergence
 
-      LogAction result = new LogAction();
-      result.setAction(Action.COMMIT);
-      result.setResult("Commit UE");
-      return result;
-    }
+    LogAction result = new LogAction();
+    result.setAction(Action.COMMIT);
+    result.setResult("Commit UE");
+    return result;
+  }
+
+  @ApiMethod(name = "action.commit.review", httpMethod = ApiMethod.HttpMethod.POST, path = "action/commit/review")
+  public LogAction commitReview(@Named("content") String content, UE ue) {
+
+    //TODO dralagen 6/4/15 : Add new review of UE
+    //TODO dralagen 6/4/15 : Log Commit review
+    //TODO dralagen 6/4/15 : Update divergence
+
+    LogAction result = new LogAction();
+    result.setAction(Action.COMMIT);
+    result.setResult("Commit Review");
+    return result;
+  }
+
+  @ApiMethod(name = "action.commit.ue", httpMethod = ApiMethod.HttpMethod.GET, path = "action/pull/{userId}")
+  public LogAction pull(@Named("userId") String userId) {
+
+    //TODO dralagen 6/4/15 : Add new version of UE
+    //TODO dralagen 6/4/15 : Log Commit UE
+    //TODO dralagen 6/4/15 : Update divergence
+
+    LogAction result = new LogAction();
+    result.setAction(Action.PULL);
+    result.setResult("Pull on user " + userId);
+    return result;
+  }
 
 
 }
