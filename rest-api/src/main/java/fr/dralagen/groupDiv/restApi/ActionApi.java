899fc50187ca755f4ab4220a50d67944b3ef962a
Dralagen <dralagen@dralagen.fr>
2015-06-04 15:49:16 +0200

    add api method commit ue

--- rest-api/src/main/java/fr/dralagen/groupDiv/restApi/ActionApi.java
+++ rest-api/src/main/java/fr/dralagen/groupDiv/restApi/ActionApi.java
@@ -1,6 +1,11 @@
 package fr.dralagen.groupDiv.restApi;
 
 import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.Named;
+import fr.dralagen.groupDiv.model.Action;
+import fr.dralagen.groupDiv.model.LogAction;
+import fr.dralagen.groupDiv.model.UE;
 
 /**
  * Created on 6/3/15.
@@ -12,10 +17,23 @@ import com.google.api.server.spi.config.Api;
 @Api(
     name = Constants.NAME,
     version = Constants.VERSION,
-    clientIds = Constants.API_ID,
+    clientIds = {Constants.API_ID},
     scopes = {}
 )
 public class ActionApi {
+
+    @ApiMethod(name = "action.commit.ue", httpMethod = ApiMethod.HttpMethod.POST, path = "action/commit/ue")
+    public LogAction commitUe(@Named("content") String content, UE ue) {
+
+      //TODO dralagen 6/4/15 : Add new version of UE
+      //TODO dralagen 6/4/15 : Log Commit UE
+      //TODO dralagen 6/4/15 : Update divergence
+
+      LogAction result = new LogAction();
+      result.setAction(Action.COMMIT);
+      result.setResult("Commit UE");
+      return result;
+    }
 
 
 }
