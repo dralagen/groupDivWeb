a24a99f44092426c08b1400e6ca8eb2f7abac6b6
Dralagen <dralagen@dralagen.fr>
2015-06-16 10:33:38 +0200

    fixes conflict when load GApi

--- rest-api/src/main/webapp/js/app.js
+++ rest-api/src/main/webapp/js/app.js
@@ -9,7 +9,12 @@ var app = angular.module("groupDiv", [
 
 app.run(['GApi', 'GAuth',
 	function(GApi, GAuth) {
-		var BASE = '//localhost:8080/_ah/api';
+		var BASE;
+		if(window.location.hostname == 'localhost') {
+			BASE = '//localhost:8080/_ah/api';
+		} else {
+			BASE = 'https://groupdivxp.appspot.com/_ah/api';
+		}
 		GApi.load('groupDivWeb','v1',BASE);
 		
 	}
