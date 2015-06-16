c80579af17666c53d8f30eb5cc5f0153e8e56f66
Dralagen <dralagen@dralagen.fr>
2015-06-17 00:25:04 +0200

    add rest methode to get a moc of divergence

---
+++ rest-api/src/main/java/fr/dralagen/groupDiv/restApi/ControlAwarnessApi.java
@@ -0,0 +1,42 @@
+package fr.dralagen.groupDiv.restApi;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.Named;
+import fr.dralagen.groupDiv.bean.DivergenceBean;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created on 6/17/15.
+ *
+ * Rest API to find all divergence value
+ *
+ * @author dralagen
+ */
+@Api(
+    name = Constants.NAME,
+    version = Constants.VERSION,
+    clientIds = {Constants.API_ID},
+    scopes = {}
+)
+public class ControlAwarnessApi {
+
+  @ApiMethod(name = "divergence", httpMethod = ApiMethod.HttpMethod.GET, path = "session/{sessionId}/divergence")
+  public DivergenceBean getDivergence(@Named("sessionId") long sessionId) {
+    DivergenceBean divergence = new DivergenceBean();
+
+    Map<Long, Integer> userDivergence = new HashMap<>();
+    int gdtot = 0;
+    for (int i = 0; i < 5; i++) {
+      int distance = (int) (Math.random()*15);
+      userDivergence.put((long) (i+1), distance);
+      gdtot+=distance;
+    }
+    divergence.setGlobalDivergence(gdtot);
+    divergence.setUserDivergence(userDivergence);
+
+    return divergence;
+  }
+}
