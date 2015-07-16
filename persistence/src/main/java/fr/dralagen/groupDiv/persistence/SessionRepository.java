0d51c3d00ada463d48ee1bf5a6799ea9c4fd6e7f
Dralagen <dralagen@dralagen.fr>
2015-07-16 11:45:06 +0200

    add memcache for Session

--- persistence/src/main/java/fr/dralagen/groupDiv/persistence/SessionRepository.java
+++ persistence/src/main/java/fr/dralagen/groupDiv/persistence/SessionRepository.java
@@ -6,6 +6,7 @@ import fr.dralagen.groupDiv.model.Session;
 
 import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -28,11 +29,20 @@ public class SessionRepository {
   }
 
   public Collection<Session> findAll () {
-    PersistenceManager pm = PMF.get().getPersistenceManager();
 
-    Query q = pm.newQuery(Session.class);
+    Collection<Session> allSession = (Collection<Session>) MemcacheRepository.getInstance().get(Session.class.getSimpleName(), "allSession");
 
-      return (Collection<Session>) q.execute();
+    if (allSession == null) {
+      PersistenceManager pm = PMF.get().getPersistenceManager();
+
+      Query q = pm.newQuery(Session.class);
+
+      allSession = (Collection<Session>) q.execute();
+
+      MemcacheRepository.getInstance().add(Session.class.getSimpleName(), "allSession", new ArrayList<>(allSession));
+    }
+
+    return allSession;
   }
 
   public Session findOne(long sessionId) {
@@ -40,16 +50,29 @@ public class SessionRepository {
   }
 
   public Session findOne(Key sessionId) {
-    PersistenceManager pm = PMF.get().getPersistenceManager();
 
-    return pm.getObjectById(Session.class, sessionId);
+    Session session = (Session) MemcacheRepository.getInstance().get(Session.class.getSimpleName(), sessionId);
 
+    if (session == null) {
+
+      PersistenceManager pm = PMF.get().getPersistenceManager();
+
+      session = pm.getObjectById(Session.class, sessionId);
+
+      MemcacheRepository.getInstance().add(Session.class.getSimpleName(), sessionId, session);
+    }
+
+    return session;
   }
 
   public Session save(Session session) {
     PersistenceManager pm = PMF.get().getPersistenceManager();
 
     session = pm.makePersistent(session);
+
+    MemcacheRepository.getInstance().clean(Session.class.getSimpleName(), "allSession");
+    MemcacheRepository.getInstance().clean(Session.class.getSimpleName(), session.getKey());
+    MemcacheRepository.getInstance().add(Session.class.getSimpleName(), session.getKey(), session);
 
     return session;
   }
